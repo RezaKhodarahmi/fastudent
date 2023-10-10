72775b7c6f57c436f17d46e4a107e231325e0bb1
RezaKhodarahmi <mrkhodarahmii@gmail.com>
2023-10-10 13:42:06 +0330

    fix iconn error

--- src/pages/app/login/index.js
+++ src/pages/app/login/index.js
@@ -93,22 +93,6 @@ const defaultValues = {
 }
 
 const LoginPage = () => {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
-
   const [rememberMe, setRememberMe] = useState(true)
   const [showPassword, setShowPassword] = useState(false)
 
