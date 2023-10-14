c9f376a7a11ebf4f9242209b2395f310e80093af
Payam <payam.mahjoub@gmail.com>
2023-10-14 16:50:29 +0330

    Latest Blog Section

--- src/pages/index.js
+++ src/pages/index.js
@@ -663,11 +663,254 @@ const Home = () => {
       </section>
 
       {/* Youtube CTA */}
-      <div className='FNV-YoutubeCTA'>
-        <div className='container'>
-          s
+      <section className='FNV-YoutubeCTA'>
+        <div className='container d-flex justify-content-center align-items-center flex-column'>
+          <svg width="42" height="30" viewBox="0 0 42 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M41.1346 4.69428C40.6507 2.85867 39.232 1.41168 37.433 0.917412C34.1463 0 20.9995 0 20.9995 0C20.9995 0 7.85323 0 4.56659 0.88263C2.80214 1.37638 1.3489 2.85894 0.864919 4.69428C0 8.04697 0 15 0 15C0 15 0 21.9881 0.864919 25.3057C1.34942 27.1411 2.76753 28.5881 4.56684 29.0823C7.88783 30 21 30 21 30C21 30 34.1463 30 37.433 29.1174C39.2323 28.6234 40.6507 27.1764 41.1352 25.341C41.9998 21.9881 41.9998 15.0353 41.9998 15.0353C41.9998 15.0353 42.0344 8.04697 41.1346 4.69428ZM16.8139 21.4235V8.57655L27.7461 15L16.8139 21.4235Z" fill="white"/>
+          </svg>
+
+            <p>
+              We at Fanavaran provide useful and diverse content for your further information<br />
+              We have collected about Canadian courses, designations and certificates.<br />
+              It is enough to visit the YouTube channel of the technicians.
+            </p>
+
+            <a href='https://www.youtube.com/channel/UCKbfvGZBXPn2Y3LGb9YDiIA' target='_blank' className='FNV-Btn BtnOutline BtnLarge'>FANAVARAN Youtube Channel</a>
         </div>
-      </div>
+      </section>
+
+      {/* Blog */}
+      <section className='FNV-LatestBlog'>
+        <h3>Latest Blogs</h3>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-12'>
+              {/* Courses Desktop */}
+              <Swiper
+                slidesPerView={3}
+                spaceBetween={10}
+                centeredSlides={false}
+                loop={true}
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false
+                }}
+                pagination={{
+                  clickable: true
+                }}
+                navigation={false}
+                breakpoints={{
+                  640: {
+                    slidesPerView: 1,
+                    spaceBetween: 20
+                  },
+                  768: {
+                    slidesPerView: 2,
+                    spaceBetween: 40
+                  },
+                  1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 50
+                  }
+                }}
+                modules={[Autoplay, Pagination, Navigation]}
+                className='FNV-NewCoursesSwiper d-none d-sm-none d-md-block'
+              >
+                <SwiperSlide>
+                  <div className='card'>
+                    <badge>درحال برگزاری</badge>
+                    <img src='img/course1.jpg' className='card-img-top' alt='...' />
+                    <div className='card-body'>
+                      <h4 className='card-title'>LEED GA Exam Preparation</h4>
+                      <price>$680.00</price>
+
+                      <div className='d-flex justify-content-between'>
+                        <Link href='#' className='FNV-Btn BtnOutline PrimaryColor BtnLarge'>
+                          See Details
+                        </Link>
+                        <Link href='#' className='FNV-Btn SecondaryColor BtnLarge'>
+                          Add to Cart
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <div className='card'>
+                    <badge>درحال ثبت نام</badge>
+                    <img src='img/course2.jpg' className='card-img-top' alt='...' />
+                    <div className='card-body'>
+                      <h4 className='card-title'>LEED GA Exam Preparation</h4>
+                      <price>$680.00</price>
+
+                      <div className='d-flex justify-content-between'>
+                        <Link href='#' className='FNV-Btn BtnOutline PrimaryColor BtnLarge'>
+                          See Details
+                        </Link>
+                        <Link href='#' className='FNV-Btn SecondaryColor BtnLarge'>
+                          Add to Cart
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <div className='card'>
+                    <badge>درحال ثبت نام</badge>
+                    <img src='img/course3.jpg' className='card-img-top' alt='...' />
+                    <div className='card-body'>
+                      <h4 className='card-title'>LEED GA Exam Preparation</h4>
+                      <price>$680.00</price>
+
+                      <div className='d-flex justify-content-between'>
+                        <Link href='#' className='FNV-Btn BtnOutline PrimaryColor BtnLarge'>
+                          See Details
+                        </Link>
+                        <Link href='#' className='FNV-Btn SecondaryColor BtnLarge'>
+                          Add to Cart
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <div className='card'>
+                    <badge>درحال ثبت نام</badge>
+                    <img src='img/course1.jpg' className='card-img-top' alt='...' />
+                    <div className='card-body'>
+                      <h4 className='card-title'>LEED GA Exam Preparation</h4>
+                      <price>$680.00</price>
+
+                      <div className='d-flex justify-content-between'>
+                        <Link href='#' className='FNV-Btn BtnOutline PrimaryColor BtnLarge'>
+                          See Details
+                        </Link>
+                        <Link href='#' className='FNV-Btn SecondaryColor BtnLarge'>
+                          Add to Cart
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              </Swiper>
+
+              <Swiper
+                slidesPerView={1}
+                spaceBetween={10}
+                centeredSlides={false}
+                loop={true}
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false
+                }}
+                pagination={{
+                  clickable: true
+                }}
+                navigation={false}
+                breakpoints={{
+                  640: {
+                    slidesPerView: 1,
+                    spaceBetween: 20
+                  },
+                  768: {
+                    slidesPerView: 2,
+                    spaceBetween: 40
+                  },
+                  1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 50
+                  }
+                }}
+                modules={[Autoplay, Pagination]}
+                className='FNV-NewCoursesSwiper d-block d-sm-block d-md-none'
+              >
+                <SwiperSlide>
+                  <div className='card'>
+                    <badge>درحال برگزاری</badge>
+                    <img src='img/course1.jpg' className='card-img-top' alt='...' />
+                    <div className='card-body'>
+                      <h4 className='card-title'>LEED GA Exam Preparation</h4>
+                      <price>$680.00</price>
+
+                      <div className='d-flex justify-content-between'>
+                        <Link href='#' className='FNV-Btn BtnOutline PrimaryColor BtnLarge'>
+                          See Details
+                        </Link>
+                        <Link href='#' className='FNV-Btn SecondaryColor BtnLarge'>
+                          Add to Cart
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <div className='card'>
+                    <badge>درحال ثبت نام</badge>
+                    <img src='img/course2.jpg' className='card-img-top' alt='...' />
+                    <div className='card-body'>
+                      <h4 className='card-title'>LEED GA Exam Preparation</h4>
+                      <price>$680.00</price>
+
+                      <div className='d-flex justify-content-between'>
+                        <Link href='#' className='FNV-Btn BtnOutline PrimaryColor BtnLarge'>
+                          See Details
+                        </Link>
+                        <Link href='#' className='FNV-Btn SecondaryColor BtnLarge'>
+                          Add to Cart
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <div className='card'>
+                    <badge>درحال ثبت نام</badge>
+                    <img src='img/course3.jpg' className='card-img-top' alt='...' />
+                    <div className='card-body'>
+                      <h4 className='card-title'>LEED GA Exam Preparation</h4>
+                      <price>$680.00</price>
+
+                      <div className='d-flex justify-content-between'>
+                        <Link href='#' className='FNV-Btn BtnOutline PrimaryColor BtnLarge'>
+                          See Details
+                        </Link>
+                        <Link href='#' className='FNV-Btn SecondaryColor BtnLarge'>
+                          Add to Cart
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                  <div className='card'>
+                    <badge>درحال ثبت نام</badge>
+                    <img src='img/course1.jpg' className='card-img-top' alt='...' />
+                    <div className='card-body'>
+                      <h4 className='card-title'>LEED GA Exam Preparation</h4>
+                      <price>$680.00</price>
+
+                      <div className='d-flex justify-content-between'>
+                        <Link href='#' className='FNV-Btn BtnOutline PrimaryColor BtnLarge'>
+                          See Details
+                        </Link>
+                        <Link href='#' className='FNV-Btn SecondaryColor BtnLarge'>
+                          Add to Cart
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
+        </div>
+      </section>
 
       <div className='FNV-FixedSocial'>
         <a href='https://www.instagram.com/fanavaran_ca/' className='FNV-Instagram'>
