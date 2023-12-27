06241fcf8d330085083b3b746c48445b48eedcff
RezaKhodarahmi <mrkhodarahmii@gmail.com>
2023-12-27 17:53:12 +0330

    certificate problem is fixed and user order is added.

--- src/pages/app/dashboards/certificates/index.js
+++ src/pages/app/dashboards/certificates/index.js
@@ -1,58 +1,183 @@
-import React, { useState, useRef, useEffect } from 'react'
-// import '../../../../../styles/Index.css' // Import your CSS file
+import React, { useState, useEffect, useRef } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getProfileInfo } from 'src/store/apps/profile'
+import {
+  Modal,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  Box,
+  CircularProgress
+} from '@mui/material'
+import { jsPDF } from 'jspdf'
 
-const Index = ({ firstName, lastName }) => {
-  const [showFullCertificate, setShowFullCertificate] = useState(false)
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 'auto',
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+  overflowY: 'auto',
+  maxHeight: '90vh'
+}
+
+const Index = () => {
+  const [open, setOpen] = useState(false)
+  const [selectedCertificate, setSelectedCertificate] = useState(null)
+  const [certificates, setCertificates] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const dispatch = useDispatch()
   const canvasRef = useRef(null)
 
-  useEffect(() => {
-    if (showFullCertificate) {
-      const canvas = canvasRef.current
-      const ctx = canvas.getContext('2d')
-      const image = new Image()
-      image.src = '/images/memberships.jpg'
-      image.onload = () => {
-        canvas.width = image.width
-        canvas.height = image.height
-        ctx.drawImage(image, 0, 0)
-        ctx.font = '60px Roboto' // Using Roboto font
-        ctx.fillText(`Reza Khodarahmi`, 1800, 400) // Adjust x, y coordinates here
-      }
-    }
-  }, [showFullCertificate, firstName, lastName])
+  // Get the profile details from the Redux store
+  const profileDetails = useSelector(state => state.profile)
 
-  const handleThumbnailClick = () => {
-    setShowFullCertificate(true)
+  useEffect(() => {
+    dispatch(getProfileInfo())
+  }, [dispatch])
+
+  function formatDate(isoDateString) {
+    const date = new Date(isoDateString)
+
+    let year = date.getFullYear()
+    let month = date.getMonth() + 1 // getMonth() returns 0-11
+    let day = date.getDate()
+
+    // Ensure month and day are 2 digits
+    month = month.toString().padStart(2, '0')
+    day = day.toString().padStart(2, '0')
+
+    return `${year}${month}${day}`
+  }
+
+  useEffect(() => {
+    if (profileDetails?.data?.courses) {
+      const courseCertificates = profileDetails.data.courses
+        .filter(course => course.certificate && course.id != 150000)
+        .map(course => ({
+          certificateUrl: course.certificate,
+          courseName: course.title,
+          userName: profileDetails.data.user.firstName,
+          lastName: profileDetails.data.user.lastName,
+          userID: profileDetails.data.user.id + formatDate(course.createdAt)
+        }))
+      setCertificates(courseCertificates)
+    }
+  }, [profileDetails])
+
+  const handleOpen = certificate => {
+    setSelectedCertificate(certificate)
+    setLoading(true)
+    setTimeout(() => {
+      if (canvasRef.current) {
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d')
+        const image = new Image()
+        image.crossOrigin = 'anonymous' // This should be set before the src attribute
+
+        image.onload = () => {
+          canvas.width = image.width
+          canvas.height = image.height
+          ctx.drawImage(image, 0, 0)
+
+          // Set font size and color for the user's name
+          ctx.font = '60px Arial'
+          ctx.fillStyle = 'black'
+          ctx.fillText(`${certificate.userName} ${certificate.lastName}`, 770, 430)
+
+          // Change font size and color for the userID
+          ctx.font = '30px Arial' // Smaller font size for userID
+          ctx.fillStyle = 'white' // Different color for userID
+          ctx.fillText(`${certificate.userID}`, 445, 832)
+
+          setLoading(false)
+        }
+
+        image.onerror = () => {
+          console.error('Failed to load image:', certificate.certificateUrl)
+          setLoading(false)
+        }
+        // image.crossOrigin = 'anonymous'
+        image.src = certificate.certificateUrl
+      }
+    }, 0)
+
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+    setLoading(false)
   }
 
   const handleDownload = () => {
-    const canvas = canvasRef.current
-    const image = canvas.toDataURL('image/png')
-    const link = document.createElement('a')
-    link.href = image
-    link.download = 'certificate.png'
-    link.click()
+    if (canvasRef.current) {
+      const canvas = canvasRef.current
+      const imgData = canvas.toDataURL('image/png')
+      const pdf = new jsPDF({
+        orientation: 'landscape',
+        unit: 'px',
+        format: [canvas.width, canvas.height]
+      })
+
+      pdf.addImage(imgData, 'PNG', 0, 0, canvas.width, canvas.height)
+      pdf.save('certificate.pdf')
+    }
   }
 
   return (
     <div>
-      <h2>Your Certificate</h2>
-      <img
-        src='path/to/certificate-thumbnail.jpg'
-        alt='Certificate Thumbnail'
-        onClick={handleThumbnailClick}
-        style={{ cursor: 'pointer' }}
-      />
+      <h2>Your Certificates</h2>
+      <TableContainer component={Paper}>
+        <Table aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Certificate Name</TableCell>
+              <TableCell align='right'>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {certificates.map((certificate, index) => (
+              <TableRow key={index} hover>
+                <TableCell component='th' scope='row'>
+                  {certificate.courseName}
+                </TableCell>
+                <TableCell align='right'>
+                  <Button variant='contained' onClick={() => handleOpen(certificate)}>
+                    Show
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
 
-      {showFullCertificate && (
-        <div className='certificate-modal'>
-          <div className='modal-content'>
-            <canvas ref={canvasRef}></canvas>
-            <button onClick={handleDownload}>Download Certificate</button>
-            <button onClick={() => setShowFullCertificate(false)}>Close</button>
-          </div>
-        </div>
-      )}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <Box sx={style}>
+          <canvas ref={canvasRef} style={{ width: '100%', height: 'auto' }}></canvas>
+          <Button onClick={handleDownload} sx={{ mt: 2 }}>
+            Download Certificate
+          </Button>
+          <Button onClick={handleClose} sx={{ mt: 2 }}>
+            Close
+          </Button>
+        </Box>
+      </Modal>
     </div>
   )
 }
--- src/pages/app/dashboards/orders/index.js
+++ src/pages/app/dashboards/orders/index.js
@@ -1,7 +1,291 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getProfileInfo } from 'src/store/apps/profile'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  CircularProgress,
+  Box,
+  Modal,
+  Typography,
+  styled
+} from '@mui/material'
+import html2canvas from 'html2canvas'
+import { jsPDF } from 'jspdf'
+
+// Styled components for the invoice
+const StyledModal = styled(Modal)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+})
+
+const ModalContent = styled(Box)({
+  backgroundColor: 'white',
+  padding: '40px',
+  outline: 'none',
+  borderRadius: '4px',
+  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  width: 'auto',
+  maxWidth: '800px',
+  maxHeight: '90vh',
+  overflowY: 'auto'
+})
+
+const InvoiceHeader = styled(Box)(({ theme }) => ({
+  fontSize: '32px',
+  fontWeight: 'bold',
+  textAlign: 'center',
+  margin: theme.spacing(2, 0),
+  paddingTop: theme.spacing(2)
+}))
+
+const Section = styled(Box)({
+  marginBottom: '24px'
+})
+
+const TwoColumnLayout = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  '& > div': {
+    width: '48%'
+  },
+  [theme.breakpoints.down('sm')]: {
+    flexDirection: 'column',
+    '& > div': {
+      width: '100%'
+    }
+  }
+}))
+
+const LineItemTable = styled(Table)({
+  marginBottom: '24px'
+})
+
+const InvoiceFooter = styled(Typography)({
+  marginTop: '24px',
+  textAlign: 'center'
+})
+
+const SignatureImage = styled('img')({
+  maxWidth: '100px',
+  marginTop: '16px'
+})
 
 const Index = () => {
-  return <div></div>
+  const [orders, setOrders] = useState([])
+  const [user, setUser] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [selectedOrder, setSelectedOrder] = useState(null)
+  const [openModal, setOpenModal] = useState(false)
+  const profileDetails = useSelector(state => state.profile)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getProfileInfo())
+  }, [dispatch])
+
+  useEffect(() => {
+    if (profileDetails?.data?.transactions) {
+      console.log(profileDetails)
+      setOrders(profileDetails.data.transactions)
+      setUser(profileDetails.data.user)
+      setLoading(false)
+    }
+  }, [profileDetails])
+
+  const handleOpenModal = order => {
+    setSelectedOrder(order)
+    setOpenModal(true)
+  }
+
+  const handleCloseModal = () => {
+    setOpenModal(false)
+  }
+
+  const downloadPDF = async () => {
+    const input = document.getElementById('modal-content')
+    const canvas = await html2canvas(input, {
+      scale: 2 // Increase the scale for better quality
+    })
+    const imgData = canvas.toDataURL('image/png')
+    const pdf = new jsPDF({
+      orientation: 'portrait',
+      unit: 'px',
+      format: 'a4'
+    })
+    const pdfWidth = pdf.internal.pageSize.getWidth()
+    const pdfHeight = pdf.internal.pageSize.getHeight()
+    pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight)
+    pdf.save('invoice.pdf')
+  }
+  if (loading) {
+    return (
+      <Box display='flex' justifyContent='center'>
+        <CircularProgress />
+      </Box>
+    )
+  }
+
+  const getStatusStyle = status => ({
+    color: status === 'succeeded' ? 'green' : 'red'
+  })
+  // Updated calculateSubtotal to use regularPrice from cycles
+  const calculateSubtotal = items => {
+    return items
+      .reduce((acc, item) => {
+        const price = item.cycles.length > 0 ? item.cycles[0].regularPrice : 0
+        return acc + parseFloat(price)
+      }, 0)
+      .toFixed(2)
+  }
+
+  // Updated renderLineItems to display regularPrice from cycles
+  const renderLineItems = items => {
+    return items.map((item, index) => (
+      <TableRow key={index}>
+        <TableCell>1</TableCell> {/* Quantity is assumed to be 1 for each course */}
+        <TableCell>{item.title}</TableCell>
+        <TableCell>${item.cycles.length > 0 ? `${item.cycles[0].regularPrice.toFixed(2)}` : 'N/A'}</TableCell>
+        <TableCell>${item.cycles.length > 0 ? `${item.cycles[0].regularPrice.toFixed(2)}` : 'N/A'}</TableCell>
+      </TableRow>
+    ))
+  }
+
+  return (
+    <Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Course</TableCell>
+              <TableCell align='right'>Transaction Date</TableCell>
+              <TableCell align='right'>Amount</TableCell>
+              <TableCell align='right'>Status</TableCell>
+              <TableCell align='right'>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orders.map(row => (
+              <TableRow key={row.Transaction_ID}>
+                <TableCell component='th' scope='row'>
+                  {row.courses.map(course => course.title).join(', ')}
+                </TableCell>
+                <TableCell align='right'>{new Date(row.Transaction_Date).toLocaleDateString()}</TableCell>
+                <TableCell align='right'>{`$${row.Amount} ${row.Currency}`}</TableCell>
+                <TableCell align='right' style={getStatusStyle(row.Transaction_Status)}>
+                  {row.Transaction_Status == 'succeeded' ? 'succeeded' : 'Canceled'}
+                </TableCell>
+                <TableCell align='right'>
+                  <Button variant='contained' onClick={() => handleOpenModal(row)}>
+                    Show
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {selectedOrder && (
+        <StyledModal open={openModal} onClose={handleCloseModal}>
+          <ModalContent id='modal-content'>
+            <InvoiceHeader>INVOICE</InvoiceHeader>
+            {/* Invoice Header - logo and invoice details */}
+            <Section>
+              <TwoColumnLayout>
+                <div>
+                  {/* Left side - Bill To */}
+                  <Typography variant='subtitle1' gutterBottom>
+                    <strong>Customer details</strong>
+                  </Typography>
+                  <Typography>Customer Name: {user?.firstName + ' ' + user?.lastName || 'John Smith'}</Typography>
+                  <Typography>Customer Address: {user?.address || '2 Court Square'}</Typography>
+                  <Typography>Customer City: {user?.city || 'Canada'}</Typography>
+                  <Typography>Customer Email: {user?.email || 'example@fanavaran.ca'}</Typography>
+                </div>
+                <div>
+                  {/* Right side - Invoice details */}
+                  <Typography variant='subtitle1' gutterBottom>
+                    <strong>Invoice </strong>
+                  </Typography>
+                  <Typography>#{selectedOrder?.Transaction_ID || 'US-001'}</Typography>
+                  <Typography style={getStatusStyle(selectedOrder.Transaction_Status)}>
+                    Status: {selectedOrder.Transaction_Status == 'succeeded' ? 'Paid' : 'Not Paid'}
+                  </Typography>
+                  <Typography variant='subtitle1' gutterBottom>
+                    <strong>Invoice Date</strong>
+                  </Typography>
+                  <Typography>
+                    {selectedOrder ? new Date(selectedOrder.Transaction_Date).toLocaleDateString() : '11/02/2019'}
+                  </Typography>
+                </div>
+              </TwoColumnLayout>
+            </Section>
+            {/* Line Items Table */}
+            <LineItemTable>
+              <TableHead>
+                <TableRow>
+                  <TableCell>QTY</TableCell>
+                  <TableCell>DESCRIPTION</TableCell>
+                  <TableCell>UNIT PRICE</TableCell>
+                  <TableCell>AMOUNT</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>{renderLineItems(selectedOrder.courses)}</TableBody>
+            </LineItemTable>
+            {/* Subtotal, Taxes, Total */}
+
+            <Section>
+              <TwoColumnLayout>
+                <div />
+                <div>
+                  <Typography variant='subtitle1' gutterBottom>
+                    <strong>Subtotal</strong>
+                  </Typography>
+                  <Typography>${calculateSubtotal(selectedOrder.courses)}</Typography>
+                  {/* No Sales Tax */}
+                  <Typography variant='subtitle1' gutterBottom>
+                    <strong>TOTAL</strong>
+                  </Typography>
+                  <Typography>${calculateSubtotal(selectedOrder.courses)}</Typography>
+                </div>
+              </TwoColumnLayout>
+            </Section>
+            {/* Signature and Footer */}
+            <Section>
+              <TwoColumnLayout>
+                <div>
+                  <Typography variant='subtitle1' gutterBottom>
+                    <strong>Terms & Conditions</strong>
+                  </Typography>
+                  <Typography>Payment is due within 15 days</Typography>
+                  <Typography>Please make checks payable to: East Repair Inc.</Typography>
+                </div>
+                <div>
+                  <SignatureImage src='/images/favicon.png' alt='Signature' />
+                </div>
+              </TwoColumnLayout>
+            </Section>
+            <InvoiceFooter variant='subtitle1'>Thank you</InvoiceFooter>
+            {/* Download as PDF Button */}
+            <Button onClick={downloadPDF} variant='contained' color='primary'>
+              Download as PDF
+            </Button>
+            {/* Close Button */}
+            <Button onClick={handleCloseModal} variant='text'>
+              Close
+            </Button>
+          </ModalContent>
+        </StyledModal>
+      )}
+    </Box>
+  )
 }
 
 export default Index
