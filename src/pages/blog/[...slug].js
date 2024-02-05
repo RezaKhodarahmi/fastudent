2aa3bd309269680ab5ebe020c06b4b7d9dcd1a8f
Payam <payam.mahjoub@gmail.com>
2024-02-05 22:14:11 +0330

    Single Blog

--- src/pages/blog/[...slug].js
+++ src/pages/blog/[...slug].js
@@ -1,14 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+
+// ** Hook Imports
+import Link from 'next/link'
 import Head from 'next/head'
+
 import { useDispatch, useSelector } from 'react-redux'
 import { getBlogWithSlug } from 'src/store/apps/blog'
 import styles from './SinglePost.module.css' // Import your CSS module here
+
+
+// ** Import Translation
+import { useTranslation } from 'react-i18next'
+
+// ** Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// ** Import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
+
+// ** Import blog section
+import SingleDeskBlog from 'src/views/blog/singleDeskPost'
+import SingleMobileBlog from 'src/views/blog/singleMobileBlog'
 
 const SinglePost = () => {
   const [post, setPost] = useState(null)
   const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
+  const { t } = useTranslation()
   const router = useRouter()
   const { slug } = router.query
   const postData = useSelector(state => state.blog)
@@ -38,11 +57,41 @@ const SinglePost = () => {
         <meta name='description' content={post.metaDescription} />
         <meta name='keywords' content={post.keywords} />
       </Head>
-      <div className={styles.postContainer}>
-        <h1 className={styles.title}>{post.title}</h1>
-        <img src={post.image} alt={post.title} className={styles.image} />
-        <div className={styles.content} dangerouslySetInnerHTML={{ __html: post.description }} />
-      </div>
+
+      <section className='FNV-Single-Post'>
+        <div className="container">
+          <div className='row'>
+            <div className='col-12 FNV-FeatureImage'>
+              <img src={post.image} alt={post.title} />
+
+              <div className='FNV-Meta'>
+                <h1>{post.title}</h1>
+
+                <span>{post.date}</span>
+              </div>
+            </div>
+
+            <div className='col-12 FNV-Content'>
+              <div dangerouslySetInnerHTML={{ __html: post.description }} />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Blog */}
+      <section className='FNV-Blog-Related'>
+        <h3>{t('Continue Reading')}</h3>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-12'>
+              {/* Blogs Desktop */}
+              {/* <SingleDeskBlog /> */}
+              {/* Blogs Mobile */}
+              {/* <SingleMobileBlog /> */}
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
