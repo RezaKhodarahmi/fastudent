1618a7f2f77ec85a1fc2f755a791d8c3654f8ad4
RezaKhodarahmi <mrkhodarahmii@gmail.com>
2024-05-07 15:45:40 +0330

    error fixed

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -37,11 +37,11 @@ const themeConfig = {
   toastPosition: 'top-center' /* top-left | top-center | top-right | bottom-left | bottom-center | bottom-right */,
   recaptchaKey: '6LdOdHgdAAAAAGWsjsBrXmsXtpTviMp6sgVlB1ty',
 
-  // stripePublicKey:
-  //   'pk_live_51HHFtaG5TuvsK0tsNRhJ7PHgkSK8BblevRKxH1fxgQfdg09Nh1VP86VJx39ZG4wf1YZVNoaicuBD9TbzqSMvpEsN00us1rpu0r' //main
-
   stripePublicKey:
-    'pk_test_51LbfVhFf0hE9RkFddCyxmVwdwjjHwrcsZafpF0NS06bhN2qMu9PGt7t5wlJDRsoGc9iQfYzebHLVtAUr6nOX4UfR00YAaWsB5d' //test
+    'pk_live_51HHFtaG5TuvsK0tsNRhJ7PHgkSK8BblevRKxH1fxgQfdg09Nh1VP86VJx39ZG4wf1YZVNoaicuBD9TbzqSMvpEsN00us1rpu0r' //main
+
+  // stripePublicKey:
+  // 'pk_test_51LbfVhFf0hE9RkFddCyxmVwdwjjHwrcsZafpF0NS06bhN2qMu9PGt7t5wlJDRsoGc9iQfYzebHLVtAUr6nOX4UfR00YAaWsB5d' //test
 }
 
 export default themeConfig
