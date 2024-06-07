1ac8e0133401eef5e320a0bd75137fac767c1c4c
Payam <w1zp3r@gmail.com>
2024-06-07 18:24:59 +0330

    New Pages and Locals Fixed

---
+++ src/pages/how-to-register/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import { Helmet } from 'react-helmet'
+import { useRouter } from 'next/router'
+
+// ** Import Translation
+import { useTranslation } from 'react-i18next'
+
+function Index() {
+//Hooks
+  const router = useRouter()
+  const { t } = useTranslation()
+
+  // Check website lang
+  useEffect(() => {
+    const lng = window.localStorage.getItem('i18nextLng')
+    if (lng == 'fa') {
+      router.push('/how-to-register/fa')
+    }
+  }, [])
+
+  return (
+    <div>index</div>
+  )
+}
+
+Index.guestGuard = true
+export default Index
