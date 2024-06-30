3ee0f508db95edfc61977ad72f404045a977d6aa
RezaKhodarahmi <mrkhodarahmii@gmail.com>
2024-06-30 17:25:52 +0330

    chat icon added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -249,9 +249,14 @@ const App = props => {
       `}
           </script>
 
-          <script type='text/javascript'>
+          {/* <script type='text/javascript'>
             {` window.$crisp=[];window.CRISP_WEBSITE_ID="55e17e3f-cbe6-4195-b704-ab9181f96363";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`}
-          </script>
+          </script> */}
+
+          <script
+            src='https://widgets.leadconnectorhq.com/loader.js'
+            data-resources-url='https://widgets.leadconnectorhq.com/chat-widget/loader.js'
+          ></script>
         </Head>
 
         <AuthProvider>
