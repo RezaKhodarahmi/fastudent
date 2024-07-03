45247a338f087f816d193af6e76b7960527f46c7
Payam <w1zp3r@gmail.com>
2024-07-03 11:25:29 +0330

    Accounting Page

---
+++ src/pages/accounting/fa/index.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import { Helmet } from 'react-helmet'
+import { useRouter } from 'next/router'
+
+// ** Import Translation
+import { useTranslation } from 'react-i18next'
+
+function Index() {
+  //Hooks
+  const router = useRouter()
+  const { t } = useTranslation()
+
+  // Check website lang
+  useEffect(() => {
+    const lng = window.localStorage.getItem('i18nextLng')
+    if (lng == 'en') {
+      router.push('/accounting')
+    }
+  }, [])
+
+  return <div>index</div>
+}
+
+Index.guestGuard = true
+
+export default Index
