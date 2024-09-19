7c08090cd7b25b1a6b652b074106a51d619b4419
Payam <w1zp3r@gmail.com>
2024-09-19 18:04:43 +0330

    Updated About

--- src/pages/hiking/index.js
+++ src/pages/hiking/index.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
-import Image from 'next/image';
+import { useEffect } from 'react';
 
-const Hiking = () => {
+function Hiking() {
+
   useEffect(() => {
     const mountainLeft = document.querySelector('#mountain_left');
     const mountainRight = document.querySelector('#mountain_right');
@@ -12,93 +12,116 @@ const Hiking = () => {
 
     const handleScroll = () => {
       let value = window.scrollY;
-      if (mountainLeft) mountainLeft.style.left = `-${value / 0.7}px`;
-      if (cloud2) cloud2.style.left = `-${value * 2}px`;
-      if (mountainRight) mountainRight.style.left = `${value / 0.7}px`;
-      if (cloud1) cloud1.style.left = `${value * 2}px`;
-      if (text) text.style.bottom = `-${value}px`;
-      if (man) man.style.height = `${window.innerHeight - value}px`;
+      mountainLeft.style.left = `-${value / 0.7}px`;
+      cloud2.style.left = `-${value * 2}px`;
+      mountainRight.style.left = `${value / 0.7}px`;
+      cloud1.style.left = `${value * 2}px`;
+      text.style.bottom = `-${value}px`;
+      man.style.height = `${window.innerHeight - value}px`;
     };
 
     window.addEventListener('scroll', handleScroll);
 
-    // Cleanup event listener on component unmount
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
-    <>
-      <section className="FNV-Hike">
-        <div className="FNV-Hike-Slider">
-          <div id="bg" className="image-container">
-            <Image
-              src="/images/hike/bg.jpg"
-              id='bg'
-              alt="Background"
-              layout="fill"
-              objectFit="cover" // Ensures it covers the entire container
-            />
-          </div>
-          <h2 id="text">HIKE</h2>
-          <div id="man" className="image-container">
-            <Image
-              src="/images/hike/man.png"
-              id='man'
-              alt="Man"
-              layout="fill"
-              objectFit="contain" // Ensures it fits inside the container
-            />
-          </div>
-          <div id="clouds_1" className="image-container">
-            <Image
-              src="/images/hike/clouds_1.png"
-              id='clouds_1'
-              alt="Cloud 1"
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-          <div id="clouds_2" className="image-container">
-            <Image
-              src="/images/hike/clouds_2.png"
-              id='clouds_2'
-              alt="Cloud 2"
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-          <div id="mountain_left" className="image-container">
-            <Image
-              src="/images/hike/mountain_left.png"
-              id='mountain_left'
-              alt="Mountain Left"
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-          <div id="mountain_right" className="image-container">
-            <Image
-              src="/images/hike/mountain_right.png"
-              id='mountain_right'
-              alt="Mountain Right"
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-        </div>
 
-        <section id="sec">
-          <h2>Welcome to the Mountains</h2>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. A earum ipsam laboriosam mollitia, architecto esse voluptates eligendi provident soluta et cupiditate sit nisi at dolorum iure dignissimos cumque amet necessitatibus blanditiis? Earum assumenda soluta reiciendis recusandae, incidunt tenetur nihil adipisci corrupti, quibusdam ullam numquam iusto veritatis facilis ab dicta, nobis inventore eius magni eveniet quo? Repellat nobis quos, facilis quam perspiciatis asperiores delectus, aliquid nihil molestias in at modi nulla minima deleniti. Minima aliquid magnam libero reiciendis et, nesciunt repellendus eum vel rerum alias ea enim fugiat eius. Quae dolores, amet nam ab officiis corrupti sequi eligendi quo culpa illum.
-          </p>
-        </section>
+    <div>
+      <section id="top" style={sectionTopStyle}>
+        <img src="https://aryan-tayal.github.io/Mountains-Parallax/bg.jpg" id="bg" style={bgStyle} />
+        <h2 id="text" style={textStyle}>Mountains</h2>
+        <img src="https://aryan-tayal.github.io/Mountains-Parallax/man.png" id="man" style={manStyle} />
+        <img src="https://aryan-tayal.github.io/Mountains-Parallax/clouds_1.png" id="clouds_1" style={cloud1Style} />
+        <img src="https://aryan-tayal.github.io/Mountains-Parallax/clouds_2.png" id="clouds_2" style={cloud2Style} />
+        <img src="https://aryan-tayal.github.io/Mountains-Parallax/mountain_left.png" id="mountain_left" style={mountainLeftStyle} />
+        <img src="https://aryan-tayal.github.io/Mountains-Parallax/mountain_right.png" id="mountain_right" style={mountainRightStyle} />
       </section>
-    </>
+      <section id="sec" style={sectionStyle}>
+        <h2>Welcome to the Mountains</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A earum ipsam laboriosam mollitia, architecto esse voluptates eligendi provident soluta et cupiditate sit nisi at dolorum iure dignissimos cumque amet necessitatibus blanditiis?
+        </p>
+      </section>
+      <footer style={footerStyle}>
+        <a href="#"><i className="fa-solid fa-house"></i></a>
+        <a href="#"><i className="fa-solid fa-mountain-sun"></i></a>
+        <a href="#"><i className="fa-solid fa-person-hiking"></i></a>
+        <a href="#"><i className="fa-solid fa-people-group"></i></a>
+        <a href="#"><i className="fa-solid fa-gears"></i></a>
+      </footer>
+    </div>
   );
-};
+}
+
+// Add guestGuard property
 Hiking.guestGuard = true;
 
 export default Hiking;
+
+// Inline styles
+const sectionTopStyle = {
+  position: 'relative',
+  height: '100vh',
+  overflow: 'hidden',
+};
+
+const bgStyle = {
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover',
+};
+
+const textStyle = {
+  position: 'absolute',
+  color: 'white',
+  fontSize: '5rem',
+  bottom: '50px',
+  left: '50px',
+};
+
+const manStyle = {
+  position: 'absolute',
+  height: '100vh',
+  zIndex: 2,
+};
+
+const cloud1Style = {
+  position: 'absolute',
+  top: '20px',
+  right: '10px',
+  zIndex: 1,
+};
+
+const cloud2Style = {
+  position: 'absolute',
+  top: '30px',
+  left: '10px',
+  zIndex: 1,
+};
+
+const mountainLeftStyle = {
+  position: 'absolute',
+  left: 0,
+  bottom: 0,
+};
+
+const mountainRightStyle = {
+  position: 'absolute',
+  right: 0,
+  bottom: 0,
+};
+
+const sectionStyle = {
+  padding: '2rem',
+  textAlign: 'center',
+};
+
+const footerStyle = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  padding: '20px',
+  backgroundColor: '#222',
+  color: 'white',
+};
